68fe663b6c32b24a788a59ea83ea4f3a94e82f04
Phis <grindchris@web.de>
2016-02-20 22:05:25 +0100

    added iterative ArrayContains method and tests passing

--- src/alias/aliasarray.php
+++ src/alias/aliasarray.php
@@ -7,7 +7,8 @@
  * @param bool $ignorecase should the function work case insensitive?
  * @return boolean
  */
-function ArrayContains($haystack, $needle, $ignorecase = false) {
+function ArrayContains($haystack, $needle, $ignorecase = false)
+{
     if (empty($haystack) || empty($needle)) {
         return false;
     }
@@ -24,6 +25,26 @@ function ArrayContains($haystack, $needle, $ignorecase = false) {
     return false;
 }
 
+function ArrayContainsIterative($haystack, $needle, $ignorecase = false)
+{
+    if (empty($haystack) || empty($needle))
+        return false;
+
+    //saves all arrays
+    $arrayPositions = [$haystack];
+
+    for ($i = 0; $i < count($arrayPositions); $i++) {
+        $currentArray = $arrayPositions[$i];
+
+        foreach ($currentArray as $value)
+            if (is_array($value))
+                $arrayPositions[] = $value;
+            else if (\Contains($value, $needle, $ignorecase))
+                return true;
+    }
+    return false;
+}
+
 /**
  * Starts a value in the array with the given needle?
  * @param array $haystack The haystack to search in
@@ -31,7 +52,8 @@ function ArrayContains($haystack, $needle, $ignorecase = false) {
  * @param bool $ignorecase should the function work case insensitive?
  * @return boolean
  */
-function ArrayStartsWith($haystack, $needle, $ignorecase = false) {
+function ArrayStartsWith($haystack, $needle, $ignorecase = false)
+{
     if (empty($haystack) || empty($needle)) {
         return false;
     }
@@ -55,12 +77,13 @@ function ArrayStartsWith($haystack, $needle, $ignorecase = false) {
  * @param bool $ignorecase should the function work case insensitive?
  * @return boolean
  */
-function ArrayEndsWith($haystack, $needle, $ignorecase = false) {
+function ArrayEndsWith($haystack, $needle, $ignorecase = false)
+{
     if (empty($haystack) || empty($needle) || !is_string($needle)) {
         return false;
-    }   
+    }
     foreach ($haystack as $value) {
-        if (is_array($value) || is_object($value)) {          
+        if (is_array($value) || is_object($value)) {
             $sub = \ArrayStartsWith($value, $needle, $ignorecase);
         } else {
             $sub = \EndsWith($value, $needle, $ignorecase);
