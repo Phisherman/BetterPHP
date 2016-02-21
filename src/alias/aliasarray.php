d1272dbb201b8e0088c503fef7245344f4c704af
squarerootfury <fury224@googlemail.com>
2016-02-21 11:35:46 +0100

    OOP overhaul

--- src/alias/aliasarray.php
+++ src/alias/aliasarray.php
@@ -1,123 +1,131 @@
 <?php
 
 /**
- * Finds a needle in a (nested) array.
- * @param array $haystack The haystack to search in
- * @param string $needle the needle to find
- * @param bool $ignorecase should the function work case insensitive?
- * @return boolean
+ * Extends the array type by useful functions
  */
-function ArrayContains($haystack, $needle, $ignorecase = false) {
-    if (empty($haystack) || empty($needle) || !is_string($needle)) {
-        return false;
-    }
-    foreach ($haystack as $value) {
-        if (is_array($value) || is_object($value)) {
-            $sub = \ArrayContains($value, $needle, $ignorecase);
-        } else {
-            $sub = \Contains($value, $needle, $ignorecase);
+class BTRArray {
+
+    /**
+     * Finds a needle in a (nested) array.
+     * @param array $haystack The haystack to search in
+     * @param string $needle the needle to find
+     * @param bool $ignorecase should the function work case insensitive?
+     * @return boolean
+     */
+    static function ArrayContains($haystack, $needle, $ignorecase = false) {
+        if (empty($haystack) || empty($needle) || !is_string($needle)) {
+            return false;
         }
-        if ($sub) {
-            return true;
-        }
-    }
-    return false;
-}
-
-/**
- * Finds a needle in an (nested) array.
- * @param array $haystack The haystack to search in
- * @param string $needle the needle to find
- * @param bool $ignorecase should the function work case insensitive?
- * @return boolean
- */
-function ArrayContainsIterative($haystack, $needle, $ignorecase = false) {
-    if (empty($haystack) || empty($needle) || !is_string($needle))
-        return false;
-
-    //saves all arrays
-    $arrayPositions = [$haystack];
-
-    for ($i = 0; $i < count($arrayPositions); $i++) {
-        $currentArray = $arrayPositions[$i];
-
-        foreach ($currentArray as $value)
-            if (is_array($value))
-                $arrayPositions[] = $value;
-            else if (\Contains($value, $needle, $ignorecase))
+        foreach ($haystack as $value) {
+            if (is_array($value) || is_object($value)) {
+                $sub = \BTRArray::ArrayContains($value, $needle, $ignorecase);
+            } else {
+                $sub = \BTRString::Contains($value, $needle, $ignorecase);
+            }
+            if ($sub) {
                 return true;
+            }
+        }
+        return false;
     }
-    return false;
-}
 
-/**
- * Starts a value in the array with the given needle?
- * @param array $haystack The haystack to search in
- * @param string $needle the needle to find
- * @param bool $ignorecase should the function work case insensitive?
- * @return boolean
- */
-function ArrayStartsWith($haystack, $needle, $ignorecase = false) {
-    if (empty($haystack) || empty($needle) || !is_string($needle)) {
-        return false;
-    }
-    foreach ($haystack as $value) {
-        if (is_array($value) || is_object($value)) {
-            $sub = \ArrayStartsWith($value, $needle, $ignorecase);
-        } else {
-            $sub = \StartsWith($value, $needle, $ignorecase);
-        }
-        if ($sub) {
-            return true;
-        }
-    }
-    return false;
-}
+    /**
+     * Finds a needle in an (nested) array.
+     * @param array $haystack The haystack to search in
+     * @param string $needle the needle to find
+     * @param bool $ignorecase should the function work case insensitive?
+     * @return boolean
+     */
+    static function ArrayContainsIterative($haystack, $needle, $ignorecase = false) {
+        if (empty($haystack) || empty($needle) || !is_string($needle))
+            return false;
 
-/**
- * Ends a value in the array with the given needle?
- * @param array $haystack The haystack to search in
- * @param string $needle the needle to find
- * @param bool $ignorecase should the function work case insensitive?
- * @return boolean
- */
-function ArrayEndsWith($haystack, $needle, $ignorecase = false) {
-    if (empty($haystack) || empty($needle) || !is_string($needle)) {
+        //saves all arrays
+        $arrayPositions = [$haystack];
+
+        for ($i = 0; $i < count($arrayPositions); $i++) {
+            $currentArray = $arrayPositions[$i];
+
+            foreach ($currentArray as $value)
+                if (is_array($value))
+                    $arrayPositions[] = $value;
+                else if (\BTRString::Contains($value, $needle, $ignorecase))
+                    return true;
+        }
         return false;
     }
-    foreach ($haystack as $value) {
-        if (is_array($value) || is_object($value)) {
-            $sub = \ArrayEndsWith($value, $needle, $ignorecase);
-        } else {
-            $sub = \EndsWith($value, $needle, $ignorecase);
+
+    /**
+     * Starts a value in the array with the given needle?
+     * @param array $haystack The haystack to search in
+     * @param string $needle the needle to find
+     * @param bool $ignorecase should the function work case insensitive?
+     * @return boolean
+     */
+    static function ArrayStartsWith($haystack, $needle, $ignorecase = false) {
+        if (empty($haystack) || empty($needle) || !is_string($needle)) {
+            return false;
         }
-        if ($sub === true) {
-            return true;
+        foreach ($haystack as $value) {
+            if (is_array($value) || is_object($value)) {
+                $sub = \BTRArray::ArrayStartsWith($value, $needle, $ignorecase);
+            } else {
+                $sub = \BTRString::StartsWith($value, $needle, $ignorecase);
+            }
+            if ($sub) {
+                return true;
+            }
         }
-    }
-    return false;
-}
-/**
- * Applies a regex search on the array values
- * @param array $haystack the array to search in
- * @param string $needle the regex
- * @return boolean
- */
-function ArrayMatch($haystack, $needle) {
-    if (empty($haystack) || empty($needle) || !is_string($needle)) {
         return false;
     }
-    foreach ($haystack as $value) {
-        if (is_array($value) || is_object($value)) {
-            $sub = \ArrayMatch($value, $needle);
-        } else {
-            $sub = \Match($value, $needle);
+
+    /**
+     * Ends a value in the array with the given needle?
+     * @param array $haystack The haystack to search in
+     * @param string $needle the needle to find
+     * @param bool $ignorecase should the function work case insensitive?
+     * @return boolean
+     */
+    static function ArrayEndsWith($haystack, $needle, $ignorecase = false) {
+        if (empty($haystack) || empty($needle) || !is_string($needle)) {
+            return false;
         }
-        if ($sub === true) {
-            return true;
+        foreach ($haystack as $value) {
+            if (is_array($value) || is_object($value)) {
+                $sub = \BTRArray::ArrayEndsWith($value, $needle, $ignorecase);
+            } else {
+                $sub = \BTRString::EndsWith($value, $needle, $ignorecase);
+            }
+            if ($sub === true) {
+                return true;
+            }
         }
+        return false;
     }
-    return false;
+
+    /**
+     * Applies a regex search on the array values
+     * @param array $haystack the array to search in
+     * @param string $needle the regex
+     * @return boolean
+     */
+    function ArrayMatch($haystack, $needle) {
+        if (empty($haystack) || empty($needle) || !is_string($needle)) {
+            return false;
+        }
+        foreach ($haystack as $value) {
+            if (is_array($value) || is_object($value)) {
+                $sub = \BTRArray::ArrayMatch($value, $needle);
+            } else {
+                $sub = \BTRString::Match($value, $needle);
+            }
+            if ($sub === true) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
 
 ?>
--- test/alias/AliasArrayTest.php
+++ test/alias/AliasArrayTest.php
@@ -3,150 +3,150 @@
 class AliasArrayTest extends PHPUnit_Framework_TestCase {
 
     public function testArrayContainsTrue() {
-        $got = \ArrayContains(array("test"), "te");
+        $got = \BTRArray::ArrayContains(array("test"), "te");
         $this->assertTrue($got);
     }
 
     public function testArrayContainsTrueDeepArray() {
         $data = array("test", array("subtest", "needle"));
-        $got = \ArrayContains($data, "needle");
+        $got = \BTRArray::ArrayContains($data, "needle");
         $this->assertTrue($got);
     }
 
     public function testArrayContainsTrueIgnoreCase() {
-        $got = \ArrayContains(array("test"), "TES", true);
+        $got = \BTRArray::ArrayContains(array("test"), "TES", true);
         $this->assertTrue($got);
     }
 
     public function testArrayContainsNeedleEmpty() {
-        $got = \ArrayContains(array("test"), "");
+        $got = \BTRArray::ArrayContains(array("test"), "");
         $this->assertFalse($got);
     }
 
     public function testArrayContainsHaystackEmpty() {
-        $got = \ArrayContains(array(), "x");
+        $got = \BTRArray::ArrayContains(array(), "x");
         $this->assertFalse($got);
     }
 
     public function testArrayContainsIterativeTrue() {
-        $got = \ArrayContainsIterative(array("test"), "te");
+        $got = \BTRArray::ArrayContainsIterative(array("test"), "te");
         $this->assertTrue($got);
     }
 
     public function testArrayContainsIterativeTrueDeepArray() {
         $data = array("test", array("subtest", "subtest"), "subtest", array("subtest", "subtest", array("subtest", "needle")));
-        $got = \ArrayContainsIterative($data, "needle");
+        $got = \BTRArray::ArrayContainsIterative($data, "needle");
         $this->assertTrue($got);
     }
 
     public function testArrayContainsIterativeTrueIgnoreCase() {
-        $got = \ArrayContainsIterative(array("test"), "TES", true);
+        $got = \BTRArray::ArrayContainsIterative(array("test"), "TES", true);
         $this->assertTrue($got);
     }
 
     public function testArrayContainsIterativeNeedleEmpty() {
-        $got = \ArrayContainsIterative(array("test"), "");
+        $got = \BTRArray::ArrayContainsIterative(array("test"), "");
         $this->assertFalse($got);
     }
 
     public function testArrayContainsIterativeHaystackEmpty() {
-        $got = \ArrayContainsIterative(array(), "x");
+        $got = \BTRArray::ArrayContainsIterative(array(), "x");
         $this->assertFalse($got);
     }
 
     public function testArrayStartsWithTrue() {
-        $got = \ArrayStartsWith(array("test"), "te");
+        $got = \BTRArray::ArrayStartsWith(array("test"), "te");
         $this->assertTrue($got);
     }
 
     public function testArrayStartsWithTrueDeepArray() {
         $data = array("test", array("subtest", "needle"));
-        $got = \ArrayStartsWith($data, "sub");
+        $got = \BTRArray::ArrayStartsWith($data, "sub");
         $this->assertTrue($got);
     }
 
     public function testArrayStartsWithTrueIgnoreCase() {
-        $got = \ArrayStartsWith(array("test"), "TE", true);
+        $got = \BTRArray::ArrayStartsWith(array("test"), "TE", true);
         $this->assertTrue($got);
     }
 
     public function testArrayStartsWithNeedleEmpty() {
-        $got = \ArrayStartsWith(array("test"), "");
+        $got = \BTRArray::ArrayStartsWith(array("test"), "");
         $this->assertFalse($got);
     }
 
     public function testArrayStartsWithHaystackEmpty() {
-        $got = \ArrayStartsWith(array(), "x");
+        $got = \BTRArray::ArrayStartsWith(array(), "x");
         $this->assertFalse($got);
     }
 
     public function testArrayEndsWithTrue() {
-        $got = \ArrayEndsWith(array("test"), "st");
+        $got = \BTRArray::ArrayEndsWith(array("test"), "st");
         $this->assertTrue($got);
     }
 
     public function testArrayEndsWithTrueDeepArray() {
         $data = array("test", array("subtest", "needle"));
-        $got = \ArrayEndsWith($data, "test");
+        $got = \BTRArray::ArrayEndsWith($data, "test");
         $this->assertTrue($got);
     }
 
     public function testArrayEndsWithTrueIgnoreCase() {
-        $got = \ArrayEndsWith(array("test"), "ST", true);
+        $got = \BTRArray::ArrayEndsWith(array("test"), "ST", true);
         $this->assertTrue($got);
     }
 
     public function testArrayEndsWithNeedleEmpty() {
-        $got = \ArrayEndsWith(array("test"), "");
+        $got = \BTRArray::ArrayEndsWith(array("test"), "");
         $this->assertFalse($got);
     }
 
     public function testArrayEndsWithHaystackEmpty() {
-        $got = \ArrayEndsWith(array(), "x");
+        $got = \BTRArray::ArrayEndsWith(array(), "x");
         $this->assertFalse($got);
     }
 
     public function testArrayEndsWithSubObject() {
         $dummy = new \stdClass();
         $dummy->foo = "dummy";
-        $got = \ArrayEndsWith(array("test", $dummy), "my");
+        $got = \BTRArray::ArrayEndsWith(array("test", $dummy), "my");
         $this->assertTrue($got);
     }
 
     public function testArrayContainsSubObject() {
         $dummy = new \stdClass();
         $dummy->foo = "dummy";
-        $got = \ArrayContains(array("test", $dummy), "mm");
+        $got = \BTRArray::ArrayContains(array("test", $dummy), "mm");
         $this->assertTrue($got);
     }
 
     public function testArrayStartsWithSubObject() {
         $dummy = new \stdClass();
         $dummy->foo = "dummy";
-        $got = \ArrayStartsWith(array("test", $dummy), "dumm");
+        $got = \BTRArray::ArrayStartsWith(array("test", $dummy), "dumm");
         $this->assertTrue($got);
     }
 
     public function testArrayMatchNeedleEmpty() {
-        $got = \ArrayMatch(array("test"), "");
+        $got = \BTRArray::ArrayMatch(array("test"), "");
         $this->assertFalse($got);
     }
 
     public function testArrayMatchHaystackEmpty() {
-        $got = \ArrayMatch(array(), "x");
+        $got = \BTRArray::ArrayMatch(array(), "x");
         $this->assertFalse($got);
     }
 
     public function testArrayMatchSubObject() {
         $dummy = new \stdClass();
         $dummy->foo = "dummy";
-        $got = \ArrayMatch(array("test", $dummy), "/my/");
+        $got = \BTRArray::ArrayMatch(array("test", $dummy), "/my/");
         $this->assertTrue($got);
     }
 
     public function testArrayMatchTrueDeepArray() {
         $data = array("test", array("subtest", "needle"));
-        $got = \ArrayMatch($data, "/[s]+/");
+        $got = \BTRArray::ArrayMatch($data, "/[s]+/");
         $this->assertTrue($got);
     }
 
